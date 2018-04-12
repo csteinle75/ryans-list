7fee911472cfe1c386f3526b697742361fb9347f
Chris Steinle <steinle.4400@gmail.com>
2018-04-12 13:49:50 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
+var logger = require('morgan');
+var mustacheExpress = require('mustache-express');
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', __dirname + '/views')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+
+
+module.exports = app;
